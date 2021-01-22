313f12c9fa4d228cec8df3f9630d007b2d5f88ce
Stanislav Fatkhutdinov <stanislav.fatkhutdinov@gmail.com>
2021-01-21 22:04:07 -0500

    add collisions

--- snake-game/main.py
+++ snake-game/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from snake import Snake
 from food import Food
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -11,6 +12,7 @@ screen.tracer(0)
 
 snake = Snake()
 food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, 'Up')
@@ -26,7 +28,22 @@ while game_is_on:
 
     # Detect collision with food
     if snake.head.distance(food) < 15:
-        print('nom nom nom')
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    # Detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    # Detect collision with tail
+    for segment in snake.segments[1:]:
+        # if segment == snake.head:
+        #     pass
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 
 screen.exitonclick()
