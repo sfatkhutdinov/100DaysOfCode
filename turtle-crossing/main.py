9d09ca0dc1408315b3c459520eb6b8a56548547a
Stanislav Fatkhutdinov <stanislav.fatkhutdinov@gmail.com>
2021-01-23 11:07:57 -0500

    turtle crossing setup

---
+++ turtle-crossing/main.py
@@ -0,0 +1,30 @@
+from turtle import Screen
+from player import Player
+from car_manager import CarManager
+from scoreboard import Scoreboard
+import time
+
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.title('Turtle Crossing')
+screen.tracer(0)
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
---
+++ turtle-crossing/scoreboard.py
@@ -0,0 +1,9 @@
+from turtle import Turtle
+
+FONT = ('Courier', 24, 'normal')
+ALIGNMENT = 'left'
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
